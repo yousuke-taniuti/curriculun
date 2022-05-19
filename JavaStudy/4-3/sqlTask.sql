22474f44b483d746240ad9db9f7a6b4d28db38dd
yousuke-taniuti <103814227+yousuke-taniuti@users.noreply.github.com>
2022-05-19 21:09:41 +0900

    課題評価テスト　訂正
    4−３　問１　訂正

--- JavaStudy/4-3/sqlTask.sql
+++ JavaStudy/4-3/sqlTask.sql
@@ -2,7 +2,7 @@
  
 -- 1. 店舗テーブルから店舗名を抽出しなさい。また、列名の表示は別名で'店舗名'とすること。
 -- [回答]
- SELECT store_name FROM  store_table 
+  SELECT store_name AS 店舗名 FROM  store_table
  
 -- 2. 店舗情報を店舗名のABC順に抽出しなさい。
 -- [回答]
